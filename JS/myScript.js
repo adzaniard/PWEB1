2d75fc612c34244d2ebe169ff77b3c5302adcc01
adzaniard <152271194+adzaniard@users.noreply.github.com>
2024-04-26 12:25:43 +0700

    Add files via upload

---
+++ JS/myScript.js
@@ -0,0 +1,6 @@
+function externalFunction() {
+    document.getElementById("demo4").innerHTML = "The paragraph has changed.";
+  }
+function externalFunction2() {
+    document.getElementById("demo5").innerHTML = "The paragraph has changed.";
+  }
